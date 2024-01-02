246e9451f9877179606ef377e8ac52b71cc6cf0a
amina.saif0 <amina.saif@pakeventures.com>
2024-01-02 16:05:04 +0500

    duplicate actions after conflict resolve removed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,19 +7,10 @@ class ProductsController < ApplicationController
     @q = params[:q]
     @products = @q.present? ? Product.search(@q) : Product.all.page(params[:page]).per(5)
   end
-  def index
-    @q = params[:q]
-    @products = @q.present? ? Product.search(@q) : Product.all.page(params[:page]).per(5)
-  end
 
   def show
   end
-  def show
-  end
 
-  def new
-    @product = Product.new
-  end
   def new
     @product = Product.new
   end
@@ -32,27 +23,10 @@ class ProductsController < ApplicationController
       render :index, status: :unprocessable_entity
     end
   end
-  def create
-    @product = Product.new(product_params)
-    if @product.save
-      redirect_to @product, notice: 'Product was successfully created.'
-    else
-      render :index, status: :unprocessable_entity
-    end
-  end
 
   def edit
   end
-  def edit
-  end
 
-  def update
-    if @product.update(product_params)
-      redirect_to @product, notice: 'Product was successfully updated.'
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
   def update
     if @product.update(product_params)
       redirect_to @product, notice: 'Product was successfully updated.'
@@ -65,23 +39,7 @@ class ProductsController < ApplicationController
     @product.destroy
     redirect_to products_url, notice: 'Product was successfully destroyed.'
   end
-  def destroy
-    @product.destroy
-    redirect_to products_url, notice: 'Product was successfully destroyed.'
-  end
 
-  def add_to_cart
-    @product = Product.find_by(params[:id])
-    current_user.create_cart if current_user.cart.nil?
-    current_user.cart.add_product(@product)
-    redirect_to products_path, notice: 'Product added to cart.'
-  end
-
-  def autocomplete
-    prefix = params[:term]
-    suggestions = Product.autocomplete_suggestions(prefix)
-    render json: suggestions and return
-  end
   def add_to_cart
     @product = Product.find_by(params[:id])
     current_user.create_cart if current_user.cart.nil?
@@ -99,23 +57,11 @@ class ProductsController < ApplicationController
   def set_product
     @product = Product.find_by(params[:id])
   end
-  private
-  def set_product
-    @product = Product.find_by(params[:id])
-  end
 
   def product_params
     params.require(:product).permit(:name, :description, :price, :category_id, :stock_quantity)
   end
-  def product_params
-    params.require(:product).permit(:name, :description, :price, :category_id, :stock_quantity)
-  end
 
-  def set_most_ordered_product
-    @most_ordered_product = Rails.cache.fetch('most_ordered_product', expires_in: 1.hour) do
-      Product.joins(:order_details).group('products.id').order('COUNT(order_details.id) DESC').first
-    end
-  end
   def set_most_ordered_product
     @most_ordered_product = Rails.cache.fetch('most_ordered_product', expires_in: 1.hour) do
       Product.joins(:order_details).group('products.id').order('COUNT(order_details.id) DESC').first
