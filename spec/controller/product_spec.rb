a8455af49f39c8dabe9cf1c4a7c2ad8ee9f1c1b2
amina.saif0 <amina.saif@pakeventures.com>
2024-01-03 12:31:25 +0500

    PC-11626 Tests related to role Product Controller in progress

--- spec/controller/product_spec.rb
+++ spec/controller/product_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe ProductsController, type: :controller do
 
   let(:category) { FactoryBot.create(:category) }
   let(:product) { FactoryBot.create(:product, category: category) }
+  let(:user) { FactoryBot.create(:user) }
+
+  before do
+    sign_in user
+  end
 
   describe 'GET #index' do
     it 'renders the index template' do
@@ -16,6 +21,13 @@ RSpec.describe ProductsController, type: :controller do
     it 'renders the show template' do
       get :show, params: { id: product.id }
       expect(response).to render_template(:show)
+    end
+  end
+
+  describe 'GET #new' do
+    it 'renders the new template' do
+      get :new
+      expect(response).to render_template(:new)
     end
   end
 end
