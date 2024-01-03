79a39e564e59a0b7b6a0cbbb8c1c708cd2d38e8a
amina.saif0 <amina.saif@pakeventures.com>
2024-01-03 16:40:21 +0500

    PC-11626 Tests related to role Order Controller in progress

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,7 +25,7 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find_by(params[:id])
+    @order = Order.find_by(id: params[:id])
     @product_details = @order.product_details
   end
 
---
+++ spec/controller/order_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe OrdersController, type: :controller do
+  let(:user) { FactoryBot.create(:user) }
+  let(:category) { FactoryBot.create(:category) }
+  let(:product) { FactoryBot.create(:product, category: category) }
+
+  before do
+    sign_in user
+  end
+end
